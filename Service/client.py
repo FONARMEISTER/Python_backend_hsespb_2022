3b9a0c7ddf04cf9517ed32e9013aeb1594c6fd77
fonarmeister <minidoc004@gmail.com>
2022-10-03 23:55:42 +0300

    working app without tests

--- Service/client.py
+++ Service/client.py
@@ -1,10 +1,20 @@
-import grpc 
+import grpc
+import sys 
 from protobuf.proto_gen.protocol_pb2_grpc import OfferSearcherStub
 from protobuf.proto_gen.protocol_pb2 import OfferFilter
 
-def search_offers(tag : str, lower_price : int, upper_price: int) -> int:
+
+def search_offers(tag_: str, lower_price_: int, upper_price_: int) -> int:
     """connects to localhost:50051 server and search for offers"""
     with grpc.insecure_channel('localhost:50051') as channel:
         client = OfferSearcherStub(channel)
-        response = client.FindOffer(request = OfferFilter(tag, lower_price, upper_price))
+        response = client.FindOffer(OfferFilter(tag=tag_, lower_price=lower_price_, upper_price=upper_price_))
         return response.in_stock
+
+
+def main():
+    print(search_offers(sys.argv[1], int(sys.argv[2]), int(sys.argv[3])))
+
+    
+if __name__ == '__main__':
+    main()
